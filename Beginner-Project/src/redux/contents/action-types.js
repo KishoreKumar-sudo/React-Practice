7cf03849cd37430f116f23dc49328f92021f1fff
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-20 13:53:50 +0530

    actiontypes created

--- Beginner-Project/src/redux/contents/action-types.js
+++ Beginner-Project/src/redux/contents/action-types.js
@@ -1,6 +1,6 @@
 //Always use a better naming convection for adding action types is that u always use UPPERCASE.
 export const Actiontypes = {
-    SET_PRODUCTS = "SET_PRODUCTS",
-    SELECTED_PRODUCTS = "SELECTED_PRODUCTS",
-    REMOVE_SELECTED_PRODUCTS = "REMOVE_SELECTED_PRODUCTS"
+    SET_PRODUCTS: "SET_PRODUCTS",
+    SELECTED_PRODUCTS: "SELECTED_PRODUCTS",
+    REMOVE_SELECTED_PRODUCTS: "REMOVE_SELECTED_PRODUCTS"
 }
