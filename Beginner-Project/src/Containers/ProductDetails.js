3ab3c8b8420b325b2e426b9a0e03930f659cfcf3
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-21 17:08:47 +0530

    params

--- Beginner-Project/src/Containers/ProductDetails.js
+++ Beginner-Project/src/Containers/ProductDetails.js
@@ -1,10 +1,21 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
 const ProductDetails = () => {
   const { productId } = useParams()
+  const dispatch = useDispatch()
   console.log(productId);
+
+  const fetchProductDetails = async () => {
+    const response = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch((err) => {
+        console.log("Err", err)
+      })
+    dispatch()
+  }
   return (
     <div>
       <h1>ProductDetails</h1>
