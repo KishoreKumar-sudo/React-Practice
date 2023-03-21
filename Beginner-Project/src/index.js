f352c1bc9e75ec8642db9968fd94f5484f3f0f95
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-21 12:48:59 +0530

    many changes were made

--- Beginner-Project/src/index.js
+++ Beginner-Project/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//1st we need to import provider which will actually provide the redux store to our react application ("react-redux package" which is a connection of a react and redux)
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './Redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
