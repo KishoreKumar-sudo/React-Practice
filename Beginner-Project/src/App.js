8b6a3c3c3d824f57edd8f8860385ae2de7d6595e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-21 14:01:26 +0530

    Routing part done

--- Beginner-Project/src/App.js
+++ Beginner-Project/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './Containers/Header';
+import ProductListing from './Containers/ProductListing';
+import ProductDetails from './Containers/ProductDetails';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact Component={ProductListing} />
+          <Route path="/product/:productId" exact Component={ProductDetails} />
+          <Route>404 Error Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
