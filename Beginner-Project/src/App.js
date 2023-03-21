8aa0949de4bbf3d16bbd6f9dd6c94d2807c22761
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-21 13:26:24 +0530

    header part done

--- Beginner-Project/src/App.js
+++ Beginner-Project/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Header from './Containers/Header';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
