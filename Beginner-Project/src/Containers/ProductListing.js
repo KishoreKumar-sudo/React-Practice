6bbdf82156429fa6f1ff287a087a44a8e135f1ef
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-21 14:12:04 +0530

    product component formed

--- Beginner-Project/src/Containers/ProductListing.js
+++ Beginner-Project/src/Containers/ProductListing.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux"
+import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
+    const products = useSelector((state) => state)
+    console.log(products)
     return (
-        <div>
-            <h1>ProductListing</h1>
+        <div className="ui grid container">
+            <ProductComponent />
         </div>
     )
 }
