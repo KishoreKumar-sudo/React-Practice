8a9c77c35fb458e334b97600c0f5375d75efb2a8
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-21 15:58:21 +0530

    map method

--- Beginner-Project/src/Containers/ProductComponent.js
+++ Beginner-Project/src/Containers/ProductComponent.js
@@ -2,19 +2,27 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products)
-    // const{id,title} = products[0];
-    return (
-        <div className="four column wide">
+    const renderList = products.map((product)=>{
+        const{id,title,image,price,category}=product
+        return(
+            <div className="four column wide">
             <div className="ui link cards">
                 <div className="card"> 
                     <div className="image"></div>
                     <div className="content">
-                        {/* <div className="header">{title}</div> */}
+                    <div className="header">{title}</div>
+                    <div className="meta price">${price}</div>
+                        <div className="meta">{category}</div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+        )
+    })
+
+    return 
+       <>{renderList}</> 
+    
 }
 
 export default ProductComponent 
