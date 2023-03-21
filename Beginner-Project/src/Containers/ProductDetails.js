bdbd3bf6ddeb98900fab6e99bea11119320b3060
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-21 19:46:13 +0530

    jsx imported

--- Beginner-Project/src/Containers/ProductDetails.js
+++ Beginner-Project/src/Containers/ProductDetails.js
@@ -5,10 +5,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import { selectedproduct } from '../Redux/Actions/productAction'
 
 const ProductDetails = () => {
-  const product=useSelector((state)=>state.product)
+  const product = useSelector((state) => state.product)
+  const { image, title, price, category, description } = product
   const { productId } = useParams()
   const dispatch = useDispatch()
-  console.log(productId);
+  console.log(product);
 
   const fetchProductDetails = async () => {
     const response = await axios
@@ -18,9 +19,39 @@ const ProductDetails = () => {
       })
     dispatch(selectedproduct(response.data))
   }
+  useEffect(() => {
+    if (productId && productId !== "") fetchProductDetails()
+  }, [productId])
   return (
-    <div>
-      <h1>ProductDetails</h1>
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${price}</a>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
