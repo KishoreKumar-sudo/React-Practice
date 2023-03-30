00af3a3e4b5e8ddb52bb6201b72a5a2233117731
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-30 17:20:34 +0530

    format operand done

--- Calculator/src/App.js
+++ Calculator/src/App.js
@@ -111,7 +111,14 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   }
   return computation.toString()
 }
-
+const INTEGER_FORMATTER=new Intl.NumberFormat("en-us",{
+  maximumFractionDigits:0,
+})
+function formatOperand(operand){
+  if(operand==null)return
+  const[integer, decimal]=operand.spilt('.')
+  if(decimal==null) return INTEGER_FORMATTER.format(integer)
+}
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
   return (
