82e5f8228ee5427b903381b8024e31d30a5d72e5
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-30 11:13:36 +0530

    renamed

---
+++ Calculator/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="calculator-grid">
+    <div className="output">
+      <div className="previous-operand"></div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
--- portfolio/src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div className="calculator-grid"></div>
-  );
-}
-
-export default App;
