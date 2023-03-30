861c3c64d7bbdc2b524d3efc9103e5f919356a2d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-30 17:31:43 +0530

    back tick for decimal object & missing another return value

--- Calculator/src/App.js
+++ Calculator/src/App.js
@@ -117,8 +117,8 @@ const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
 function formatOperand(operand) {
   if (operand == null) return
   const [integer, decimal] = operand.split(".")
-  if (decimal == null) 
-  return `${INTEGER_FORMATTER.format(integer)}`
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
