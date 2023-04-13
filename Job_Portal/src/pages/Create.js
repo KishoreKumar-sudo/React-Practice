0c0743df17941c4209a897c486619d0795d155b7
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-13 17:49:18 +0530

    21

--- Job_Portal/src/pages/Create.js
+++ Job_Portal/src/pages/Create.js
@@ -8,26 +8,26 @@ import {
   Box,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-const initial = { profile: "", exp: 0, techs: [], desc:"" };
+const initial = { profile: "", exp: 0, techs: [], desc: "" };
 
 const Create = () => {
-    const skillSet = [
-        {
-          name: "Javascript"
-        },
-        {
-          name: "Java"
-        },
-        {
-          name: "Python"
-        },
-        {
-          name: "Django"
-        },
-        {
-          name: "Rust"
-        }
-      ];
+  const skillSet = [
+    {
+      name: "Javascript"
+    },
+    {
+      name: "Java"
+    },
+    {
+      name: "Python"
+    },
+    {
+      name: "Django"
+    },
+    {
+      name: "Rust"
+    }
+  ];
   const navigate = useNavigate();
   const [form, setForm] = useState(initial);
 
@@ -47,17 +47,17 @@ const Create = () => {
       .catch((error) => {
         console.error("Error:", error);
       });
-      navigate('/employee/feed');
+    navigate('/employee/feed');
   };
 
   const { profile, exp, desc } = form;
 
   const handleChange = (e) => {
-    setForm({...form , techs : [...form.techs, e.target.value]});
+    setForm({ ...form, techs: [...form.techs, e.target.value] });
   }
 
   return (
-    <Paper sx={{ padding:"2%"}} elevation={3}>
+    <Paper sx={{ padding: "2%" }} elevation={3}>
       <Typography sx={{ margin: "3% auto" }} align="center" variant="h5">
         Create New Post
       </Typography>
@@ -88,7 +88,7 @@ const Create = () => {
             variant="outlined"
             value={exp}
           />
-           <TextField
+          <TextField
             type="string"
             sx={{ width: "50%", margin: "2% auto" }}
             required
@@ -99,29 +99,29 @@ const Create = () => {
             variant="outlined"
             value={desc}
           />
-          <Box sx={{ margin:"1% auto"}}>
-          <h3>Please mention required skills</h3>
-         <ul>
-        {skillSet.map(({ name }, index) => {
-          return (
-            <li key={index}>
-              <div >
-                <div>
-                  <input
-                    type="checkbox"
-                    id={`custom-checkbox-${index}`}
-                    name={name}
-                    value={name}
-                    onChange={handleChange}  
-                  />
-                  <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
-                </div>
-              </div>
-            </li>
-          );
-        })}
-       
-      </ul>
+          <Box sx={{ margin: "1% auto" }}>
+            <h3>Please mention required skills</h3>
+            <ul>
+              {skillSet.map(({ name }, index) => {
+                return (
+                  <li key={index}>
+                    <div >
+                      <div>
+                        <input
+                          type="checkbox"
+                          id={`custom-checkbox-${index}`}
+                          name={name}
+                          value={name}
+                          onChange={handleChange}
+                        />
+                        <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+
+            </ul>
           </Box>
           <Button
             sx={{ width: "50%", margin: "2% auto" }}
