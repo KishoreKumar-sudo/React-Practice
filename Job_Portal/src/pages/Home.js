5b109a746266db04fe7fe35394dc12430fb1da8a
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-13 17:49:48 +0530

    22

--- Job_Portal/src/pages/Home.js
+++ Job_Portal/src/pages/Home.js
@@ -7,23 +7,23 @@ import "../App.css"
 const Home = () => {
   return (
     <div>
-      <Typography sx={{ margin:"5%" }} variant="h3" align="center">
+      <Typography sx={{ margin: "5%" }} variant="h3" align="center">
         Get Hired or Hire people for free!
       </Typography>
       <div>
         <ul className="ul">
           <li>
-          <Button sx={{ margin:"2% 3%"}} variant="outlined">
-            <Link to="/employer/dashboard">
-              Hire talent
-            </Link>
+            <Button sx={{ margin: "2% 3%" }} variant="outlined">
+              <Link to="/employer/dashboard">
+                Hire talent
+              </Link>
             </Button>
           </li>
           <li>
-          <Button sx={{ margin:"2% 3%"}} variant="outlined">
-            <Link to="/employee/feed">
-              Get Job Now
-            </Link>
+            <Button sx={{ margin: "2% 3%" }} variant="outlined">
+              <Link to="/employee/feed">
+                Get Job Now
+              </Link>
             </Button>
           </li>
         </ul>
