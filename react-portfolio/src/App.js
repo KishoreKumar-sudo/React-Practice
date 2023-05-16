f689cb80b5b7f98577a0ebd366eb8315801e40fd
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-16 16:25:13 +0530

    2

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/experience" element={<Experience />}/>
+        </Routes>
+      </Router> 
+    </div>
   )
 }
 
